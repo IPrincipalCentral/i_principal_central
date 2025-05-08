70cd7bd73bf7e61857df3b42279aae5931570b2a
IPrincipalCentral <deboubil21@gmail.com>
2025-05-08 13:40:13 +0100

    Add files via upload

---
+++ i_main_0.py
@@ -0,0 +1,47 @@
+
+
+
+
+
+
+
+
+
+
+
+
+import i_principal_central
+
+
+
+global i
+
+
+i = {}
+
+i["i_class"] = i_principal_central.i_class()
+
+i["i_class"].i_am_you()
+
+i["i_class"].i_develope()
+
+
+i["i_class"].i_function("i")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
